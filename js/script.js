bbcb242246b964e37591d1dadae1ac300d09d3ab
Sukhpreet Saini <sukhpreet.saini2020@gmail.com>
2023-11-17 21:59:37 -0500

    updated with the total cost

--- js/script.js
+++ js/script.js
@@ -19,11 +19,18 @@ function submitForm() {
         }
     }
 
+
     // Create a Pizza object
     var pizzaOrder = new Pizza(fullName, email, contactNo, pizzaType, pizzaSize, crustType, toppings, delivery, deliveryAddress, paymentMethod);
 
     // Output description of the customer's pizza to the HTML page
     document.getElementById("studentInfo").innerHTML = "Student ID: 200520246<br>Name: Sukhpreet Saini<br><br>" + pizzaOrder.getDescription();
+
+    // Calculate total cost
+    const totalCost = pizzaOrder.calculateTotalCost();
+
+    // Display total cost
+    document.getElementById("totalCost").innerHTML = `Total Cost: $${totalCost.toFixed(2)}`;
 
     // Display submission message
     document.getElementById("submissionMessage").innerHTML = "Order submitted successfully!";
@@ -47,4 +54,34 @@ class Pizza {
     getDescription() {
         return `Pizza Order:\n${this.pizzaSize} ${this.pizzaType} pizza with ${this.crustType} crust and toppings: ${this.toppings.join(", ")}.\nDelivery: ${this.delivery}, Address: ${this.deliveryAddress}\nPayment Method: ${this.paymentMethod}`;
     }
+    calculateTotalCost() {
+        let baseCost = 0;
+
+        // Set base cost based on pizza size
+        switch (this.pizzaSize) {
+            case "small":
+                baseCost = 8.99;
+                break;
+            case "medium":
+                baseCost = 11.99;
+                break;
+            case "large":
+                baseCost = 14.99;
+                break;
+            default:
+                break;
+        }
+
+        // Add additional cost for each topping
+        const toppingCost = 1.5 * this.toppings.length;
+
+        // Add delivery cost if delivery is selected
+        const deliveryCost = this.delivery === "yes" ? 2.99 : 0;
+
+        // Calculate total cost
+        const totalCost = baseCost + toppingCost + deliveryCost;
+
+        return totalCost;
+    }
 }
+
